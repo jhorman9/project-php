670cdc2131d8150f5a0f5a2342ac8fe5ef6f7e20
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2024-02-05 16:50:15 -0500

    creating header and footer php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php  include('db.php') ?>
+
+<?php include('includes/header.php') ?>
+
+    <div class='container p-4'>
+        <div class='row'>
+            
+            <div class='col-md-4'>
+                <div class='card card-body'>
+                    <form action="">
+                        <div class="form-group">
+                            <input type="text" name="title" class='form-control' placeholder="Task title" autofocus>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    
+        </div>
+    </div>
+
+
+<?php include('includes/footer.php') ?>
